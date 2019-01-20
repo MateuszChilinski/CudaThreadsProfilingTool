bb09bc2f32f6ed5a049eebd609a8e492275134fc
Marcin <marcin.kochanowski96@gmail.com>
2019-01-20 22:32:10 +0100

    remove old analitic module

--- Analitic Module/application/main_window.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-from tkinter import ttk
-from .plot_window import PlotWindow
-
-
-class MainWindow(Frame):
-    def __init__(self, master):
-        Frame.__init__(self, master)
-        PlotWindow(master)
--- Analitic Module/application/tests/test_naming_conventions.md
+++
@@ -1,2 +0,0 @@
-# all files should match expression: test*.py
-# all test methods should start with 'test'
